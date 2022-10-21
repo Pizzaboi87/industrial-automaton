0aefa977f98cebb8eae2c02136c93beca835fc89
Peter Weiser <peterweiser87@gmail.com>
2022-10-21 16:10:18 +0300

    Loading page added.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import Door from './components/Door';
-import Header from './components/Header'
-import Main from './components/Main';
+import { Suspense } from 'react';
+import React from 'react';
 
-function App() {
+const Door = React.lazy(() => import('./components/Door'));
+const Header = React.lazy(() => import('./components/Header'));
+const Main = React.lazy(() => import('./components/Main'))
+
+const App = () => {
   return (
-    <div className="App">
-      <Door />
-      <Header />
-      <Main />
+    <div>
+      <Suspense fallback={
+        <div className="loading">
+          <img src="../images/bb8.gif" alt="loading" />
+          <h1>Loading...</h1>
+        </div>}>
+        <div className='App'>
+          <Door />
+          <Header />
+          <Main />
+        </div>
+      </Suspense>
     </div>
   );
 }

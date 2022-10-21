7fdd9f14de6c4f284bd4d16f9e69d39fc3c52644
Peter Weiser <peterweiser87@gmail.com>
2022-10-21 18:08:47 +0300

    Loading page settings.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Main = React.lazy(() => import('./components/Main'))
 let fullfilled = false;
 let promise = null;
 const useTimeout = (ms) => {
-    if (fullfilled && document.readyState === "complete") {
+    if (!fullfilled && document.readyState === "complete") {
         throw promise || (promise = new Promise((res) => {
             setTimeout(() => {
                 fullfilled = true;
@@ -19,7 +19,7 @@ const useTimeout = (ms) => {
     }
 };
 const Test = () => {
-    useTimeout(2000);
+    useTimeout(4000);
     return (
       <div className="App">
         <Door />
